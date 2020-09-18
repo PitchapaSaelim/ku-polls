6f76b24f1aa018aebf085ab26a31b904fef3a915
PitchapaSaelim <pitchapa.sae@ku.th>
2020-09-18 23:04:25 +0700

    Improve navigation

--- polls/views.py
+++ polls/views.py
@@ -19,7 +19,7 @@ class IndexView(generic.ListView):
         """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        ).order_by('-pub_date')
 
 
 class DetailView(generic.DetailView):
@@ -63,5 +63,4 @@ def vote_for_poll(request, pk):
         messages.error(request, f"Voting is not allowed.")
         return redirect('polls:index')
     
-    messages.success(request, "Your voted successfully recorded. Thank you.")
     return render(request,'polls/detail.html',{'question':question})
