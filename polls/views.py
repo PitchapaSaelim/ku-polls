cf25d03d0d4f5a30e65e7061c3de2b099ba048e2
PitchapaSaelim <pitchapa.sae@ku.th>
2020-09-18 22:07:53 +0700

    Redirect web visitors and show an error message where voting is not allowed

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.contrib import messages
 
 from .models import Choice, Question
 
@@ -54,3 +55,13 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+        
+def vote_for_poll(request, pk):
+    question = get_object_or_404(Question, pk=pk)
+    if not question.can_vote():
+        messages.error(request, f"Voting is not allowed.")
+        return redirect('polls:index')
+    
+    messages.success(request, "Your voted successfully recorded. Thank you.")
+    return render(request,'polls/detail.html',{'question':question})
