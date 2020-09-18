34a901d1364fa4a364f829829a904f2a7badba24
PitchapaSaelim <pitchapa.sae@ku.th>
2020-09-18 20:16:26 +0700

    Create unit tests for is_published and can_vote

--- polls/tests.py
+++ polls/tests.py
@@ -36,14 +36,63 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
+    def test_is_published_with_current_date_is_on_publication_date(self):
+        """
+        is_published() returns True if current date is on question’s publication date.
+        """
+        time = timezone.now()
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.is_published(), True)
+
+    def test_is_published_with_current_date_is_after_publication_date(self):
+        """
+        is_published() returns True if current date is after question’s publication date.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.is_published(), True)
+
+    def test_is_published_with_current_date_is_before_publication_date(self):
+        """
+        is_published() returns False if current date is before question’s publication date.
+        """
+        time = timezone.now() + datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.is_published(), False)
+
+    def test_can_vote_with_current_date_is_before_end_date(self):
+        """
+        can_vote() returns True if voting is currently allowed for this question before the end date.
+        """
+        time = timezone.now() + datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(end_date=time)
+        self.assertIs(recent_question.can_vote(), True)
+
+    def test_can_vote_with_current_date_is_on_end_date(self):
+        """
+        can_vote() returns True if voting is currently allowed for this question on the end date.
+        """
+        time = timezone.now()
+        recent_question = Question(end_date=time)
+        self.assertIs(recent_question.can_vote(), True)
+ 
+    def test_can_vote_with_current_date_is_after_end_date(self):
+        """
+        can_vote() returns False if voting is currently allowed for this question after the end date.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(end_date=time)
+        self.assertIs(recent_question.can_vote(), False)
+
 def create_question(question_text, days):
     """
     Create a question with the given question_text and published the
     given number of days offset to now (negative for questions published
     in the past, positive for questions that have yet to be published).
     """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+    time_of_pub_date = timezone.now() + datetime.timedelta(days=days)
+    time_of_end_date = datetime.timedelta(hours=23, minutes=59, seconds=59) + time_of_pub_date
+    return Question.objects.create(question_text=question_text, pub_date=time_of_pub_date, end_date=time_of_end_date)
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
